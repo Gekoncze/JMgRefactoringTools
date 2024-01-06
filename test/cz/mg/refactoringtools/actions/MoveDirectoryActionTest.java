0e997a6f96e87eaa13d4ccb4aa1b6189c25f9b38
Mg <gekoncze@seznam.cz>
2024-01-06 22:10:21 +0100

    added MoveDirectoryAction test

---
+++ test/cz/mg/refactoringtools/actions/MoveDirectoryActionTest.java
@@ -0,0 +1,96 @@
+package cz.mg.refactoringtools.actions;
+
+import cz.mg.annotations.classes.Test;
+import cz.mg.annotations.requirement.Mandatory;
+import cz.mg.collections.list.List;
+import cz.mg.file.page.Page;
+import cz.mg.test.Assert;
+
+import java.nio.file.Path;
+
+public @Test class MoveDirectoryActionTest {
+    public static void main(String[] args) {
+        System.out.print("Running " + MoveDirectoryActionTest.class.getSimpleName() + " ... ");
+
+        MoveDirectoryActionTest test = new MoveDirectoryActionTest();
+        test.testRun();
+
+        System.out.println("OK");
+    }
+
+    private void testRun() {
+        Assert.assertThatCollections(
+            new List<>(
+                "package cz.mg.foobar",
+                "",
+                "import com.example.foo",
+                "import eu.mg.collections.List",
+                "import eu.mg.collections.Array",
+                "import eu.mg.foobar.Foo",
+                "",
+                "public class FooBar {",
+                "}"
+            ),
+            testRun(new MoveDirectoryAction(
+                "cz",
+                "eu"
+            ))
+        ).areEqual();
+
+        Assert.assertThatCollections(
+            new List<>(
+                "package cz.mg.foobar",
+                "",
+                "import com.example.foo",
+                "import cz.mg.data.List",
+                "import cz.mg.data.Array",
+                "import cz.mg.foobar.Foo",
+                "",
+                "public class FooBar {",
+                "}"
+            ),
+            testRun(new MoveDirectoryAction(
+                "cz.mg.collections",
+                "cz.mg.data"
+            ))
+        ).areEqual();
+
+        Assert.assertThatCollections(
+            new List<>(
+                "package cz.mg.foobar",
+                "",
+                "import com.example2.foo",
+                "import cz.mg.collections.List",
+                "import cz.mg.collections.Array",
+                "import cz.mg.foobar.Foo",
+                "",
+                "public class FooBar {",
+                "}"
+            ),
+            testRun(new MoveDirectoryAction(
+                "com.example",
+                "com.example2"
+            ))
+        ).areEqual();
+    }
+
+    private @Mandatory List<String> testRun(@Mandatory FileAction action) {
+        Page page = createTestPage();
+        action.run(page);
+        return page.getLines();
+    }
+
+    private @Mandatory Page createTestPage() {
+        return new Page(Path.of("test.java"), new List<>(
+            "package cz.mg.foobar",
+            "",
+            "import com.example.foo",
+            "import cz.mg.collections.List",
+            "import cz.mg.collections.Array",
+            "import cz.mg.foobar.Foo",
+            "",
+            "public class FooBar {",
+            "}"
+        ));
+    }
+}
